6dd4938d2a73673899e244f05199aaa8e9b1130f
AswinBarath <aswin2001barath@gmail.com>
2021-03-11 16:39:07 +0530

    Updating database 2

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-slippery-03283',
-      user : 'egwkazehcilzql',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
